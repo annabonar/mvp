44d39ffef517523f20a4e0a57e04a298fc958140
Yih-Fann Chen <chenyih@oa-j318-04.ddi.sheridanc.on.ca>
2014-03-06 16:13:51 -0500

    test commit

---
+++ application/views/test_commit.php
@@ -0,0 +1,9 @@
+<div id="mainarea">
+	<h2>Success!</h2> 
+		<p>You successfully deleted a new typeface listing.</p>
+		<p>Head back to the main page to see the new listing.</p>
+
+		<a id="hidden" href="http://phoenix.sheridanc.on.ca/~ccit1559/codeigniter/index.php">
+			<button type="submit" name="submit" value="Back"><< Back to Main Page
+			</button>
+		</a>
