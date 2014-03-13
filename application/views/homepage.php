e2ae65922be9d7635673f8b804aa82f4c688be24
Yih-Fann Chen <chenyih@oa-j318-04.ddi.sheridanc.on.ca>
2014-03-13 16:39:05 -0400

    really rough homepage

---
+++ application/views/homepage.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style2.css">
+		<title>UTMFoodies</title>
+	</head>
+
+	<body>
+
+		<div id="main">
+			<div id="header">
+				<h1>UTMFoodies</h1>
+			</div>
+
+
+			<div id="navbar">
+				<nav>
+					<ul>
+						<li class="navbtn"><a href="/find">Find Restaurant</a></li>
+						<li class="navbtn"><a href="/add">Add Review</a></li>
+						<li class="navbtn"><a href="/map">Map</a></li>
+						<li><input type="text" id="searchbox" name="search" placeholder="Search"><input type="button" name="searchbtn" value="Q"></li>
+					</ul>
+				</nav>
+			</div> 
+
+			<hr>
+
+			<div class="restListings">
+				<div class="row" id="row1">
+					<div class="rowElement" id="boosterjuice">Booster Juice</div>
+					<div class="rowElement" id="elements">Elements</div>
+					<div class="rowElement" id="internationalkitchen">International Kitchen</div>
+					<div class="rowElement" id="pizzapizza">Pizza Pizza</div>
+					<div class="rowElement" id="tandoriindiancuisine">Tandori Indian Cuisine</div>
+				</div>
+				<div class="row" id="row2">
+					<div class="rowElement" id="delistation">Deli Station</div>
+					<div class="rowElement" id="timhortonsselfserve">Tim Hortons Self Serve</div>
+					<div class="rowElement" id="secondcup">Second Cup</div>
+					<div class="rowElement" id="subway">Subway</div>
+					<div class="rowElement" id="timhortons_south">Tim Hortons - Davis Building</div>
+				</div>
+				<div class="row" id="row3">
+					<div class="rowElement" id="circuitbreakcafe">Circuit Break Cafe</div>
+					<div class="rowElement" id="secondcupandpaninifresca">Second Cup & Panini Fresca</div>
+					<div class="rowElement" id="mikesdoghouse">Mike's Doghouse</div>
+					<div class="rowElement" id="starbucks">Starbucks</div>
+					<div class="rowElement" id="timhortons_north">Tim Hortons - North Building</div>
+				</div>
+				<div class="row" id="row4">
+					<div class="rowElement" id="colmancommons">Colman Commons Dining Hall</div>
+					<div class="rowElement" id="blindduckpub">Blind Duck Pub</div>
+				</div>
+			</div>
+
+			<div id="footer">
+			</div>	
+		</div>
+	</body>
+</html>
